9ab0315a39b75e148a2d1c3c988442ecc589c775
matheusrgomess <matheusdrgomes@gmail.com>
2024-11-26 23:57:46 -0300

    fix merge

--- src/topics/products/modals/modalBasic.jsx
+++
@@ -1,5 +0,0 @@
-export default function ModalBasic () {
-    return (
-        <div></div>
-    )
-}
--- src/topics/products/modals/modalPlus.jsx
+++
@@ -1,5 +0,0 @@
-export default function ModalPlus () {
-    return (
-        <div></div>
-    )
-}
